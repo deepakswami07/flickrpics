aa4f704f99b54d3f736f7493bf21ddd10a9ec80b
daniel <deepakswami07@gmail.com>
2015-02-25 00:05:23 -0500

    fix tag bug

--- js/flickr.js
+++ js/flickr.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
+	
 	$('form').submit(function(event){
 		event.preventDefault();
-		$searchField = $('#search');
+		console.log("hello");
 	
+		var $searchField = $('#search');
+		var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+		var animal = $searchField.val();
+		console.log(animal);
+		console.log("hello");
 
-var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-var animal = $searchField.val();
+		var flickrOptions = {
+      		 tags: animal,
+        	 format: "json"
+    	};
+    	console.log(flickrOptions);
 
-var flickrOptions = {
-	tag: animal,
-	format: "json"
-}
-
-function flickrImages(data){
+	function flickrImages(data){
 	var photoHTML = "";
+	if (data.items.length > 0) {
 	photoHTML =+ '<div class="container">';
 	photoHTML =+ '<div class="rows">';
 	photoHTML += '<div class="jumbotron">';
@@ -24,8 +29,12 @@ function flickrImages(data){
 		photoHTML += '<img src=" ' + photos.media.m + ' ">';
 		photoHTML += '</a>';
 		photoHTML += '</li>';
-
 	});
+
+		} else {
+		photoHTML =  photoHTML = '<p id="noMatch" class="lead text-danger"> No photos found to match your search word: ' + animal + '.</p>'
+	}
+
 	photoHTML += '</ul>';
 	photoHTML += '</div>';
 	photoHTML += '</div>';
