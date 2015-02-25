dd8024937073d56f5cdc79766a90f50bb2c7acd6
daniel <deepakswami07@gmail.com>
2015-02-25 14:17:47 -0500

    fix bugs in getjson callback

--- js/flickr.js
+++ js/flickr.js
@@ -1,54 +1,48 @@
 $(document).ready(function(){
 	
-	$('form').submit(function(event){
-		event.preventDefault();
+var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 
-		console.log("hello");
-		console.log($searchField);
+$('form').submit(function(event){
+	var $searchField = $("#search");
 
+	event.preventDefault();
 
-		var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-		var animal = $searchField.val();
+	var animal = $searchField.val();
+	var flickrOptions = {
+	tags: animal,
+	format: "json"
+	}
 
-		var flickrOptions = {
-		tags: animal,
-		format: "json"
-		}
+	var $searchField = $('#search');
+	var animal = $searchField.val();
 
-function flickrImages(data){
-	var photoHTML = " ";
-
-		var $searchField = $('#search');
-		var animal = $searchField.val();
-		var flickrOptions = {
-      		 tags: animal,
-        	 format: "json"
+	var flickrOptions = {
+      	tags: animal,
+        format: "json"
     	};
+
 	function flickrImages(data){
-	var photoHTML = "";
-	if (data.items.length > 0) {
-		photoHTML = '<ul>';
-		$.each(data.items, function(i, photos){
-		photoHTML += '<li class="col-sm-3 col-sm-3">';
-		photoHTML += '<a href=" ' + photos.link + ' " class="image">';
-		photoHTML += '<img src=" ' + photos.media.m + ' ">';
-		photoHTML += '</a>';
-		photoHTML += '</li>';
-	});
+		var photoHTML = "";
+		if (data.items.length > 0) {
+			photoHTML = '<ul>';
+			$.each(data.items, function(i, photos){
+			photoHTML += '<li class="col-sm-3 col-sm-3">';
+			photoHTML += '<a href=" ' + photos.link + ' " class="image">';
+			photoHTML += '<img src=" ' + photos.media.m + ' ">';
+			photoHTML += '</a>';
+			photoHTML += '</li>';
+			});
 
 		} else {
 		photoHTML =  photoHTML = '<p id="noMatch" class="lead text-danger"> No photos found to match your search word: ' + animal + '.</p>'
-	}
+		}
 
 		photoHTML += '</ul>';
 
-	$("#photos").html(photoHTML);
-	}
+		$("#photos").html(photoHTML);
+		}
 
-	$.getJSON(flickrAPI, flickrOptions, flickrImages );
+		$.getJSON(flickrAPI, flickrOptions, flickrImages );
 	});
-
-
-});
 });
 
