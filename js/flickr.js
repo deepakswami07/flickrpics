662d2fd28587e1a412a2b8b86c8c2a9e250e109f
daniel <deepakswami07@gmail.com>
2015-02-25 00:49:55 -0500

    center footer css - refactor html & js

--- js/flickr.js
+++ js/flickr.js
@@ -2,41 +2,23 @@ $(document).ready(function(){
 	
 	$('form').submit(function(event){
 		event.preventDefault();
-		console.log("hello");
-		console.log($searchField);
-
-
-		var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-		var animal = $searchField.val();
-
-		var flickrOptions = {
-		tags: animal,
-		format: "json"
-		}
-
-function flickrImages(data){
-	var photoHTML = "
+		
 		var $searchField = $('#search');
 		var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 		var animal = $searchField.val();
-		console.log(animal);
-		console.log("hello");
-
 		var flickrOptions = {
       		 tags: animal,
         	 format: "json"
     	};
-    	console.log(flickrOptions);
-
 	function flickrImages(data){
 	var photoHTML = "";
 	if (data.items.length > 0) {
-	photoHTML =+ '<div class="container">';
-	photoHTML =+ '<div class="rows">';
-	photoHTML += '<div class="jumbotron">';
+		photoHTML =+ '<div class="container">';
+		photoHTML =+ '<div class="rows">';
+		photoHTML += '<div class="jumbotron">';
+		photoHTML = '<ul>';
 
-	photoHTML = '<ul>';
-	$.each(data.items, function(i, photos){
+		$.each(data.items, function(i, photos){
 		photoHTML += '<li class="col-sm-3 col-sm-3">';
 		photoHTML += '<a href=" ' + photos.link + ' " class="image">';
 		photoHTML += '<img src=" ' + photos.media.m + ' ">';
@@ -48,7 +30,11 @@ function flickrImages(data){
 		photoHTML =  photoHTML = '<p id="noMatch" class="lead text-danger"> No photos found to match your search word: ' + animal + '.</p>'
 	}
 
-	photoHTML += '</ul>';
+		photoHTML += '</ul>';
+		photoHTML += '</div>';
+		photoHTML += '</div>';
+		photoHTML += '</div>';
+
 	$("#photos").html(photoHTML);
 	}
 
@@ -56,7 +42,3 @@ function flickrImages(data){
 	});
 
 });
-
-
-
-
