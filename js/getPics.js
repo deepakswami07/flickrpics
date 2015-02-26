101487083037fafe0575668c82070d3438e52cd2
daniel <deepakswami07@gmail.com>
2015-02-25 19:16:01 -0500

    added search note and style it

--- js/getPics.js
+++ js/getPics.js
@@ -7,6 +7,8 @@ var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncal
 
 	var $searchField = $("#search");
 
+
+
 	event.preventDefault();
 	var animal = $searchField.val();
 	var flickrOptions = {
@@ -14,7 +16,6 @@ var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncal
 	format: "json"
 	}
 
-	var $searchField = $('#search');
 	var animal = $searchField.val();
 
 	var flickrOptions = {
@@ -54,54 +55,57 @@ var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncal
 
 // Begin code for friends pics  //
 
-var searchPictures = function(){
+// var friendsPictures = function(){
 
-var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-	$('form').submit(function(event){
+// var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+// 	$('form').submit(function(event){
 
-	var $searchField = $("#search");
+// 	var $searchField = $("#search");
 
-	event.preventDefault();
-	var animal = $searchField.val();
-	var flickrOptions = {
-	tags: animal,
-	format: "json"
-	}
+// 	event.preventDefault();
+// 	var animal = $searchField.val();
+// 	var flickrOptions = {
+// 	tags: animal,
+// 	format: "json"
+// 	}
 
-	var $searchField = $('#search');
-	var animal = $searchField.val();
+// 	var $searchField = $('#search');
+// 	var animal = $searchField.val();
 
-	var flickrOptions = {
-      	tags: animal,
-        format: "json"
-    	};
+// 	var flickrOptions = {
+//       	tags: animal,
+//       	tagmode: "all",
+//         format: "json"
+//     	};
 
-	function flickrImages(data){
-		var photoHTML = "";
-		if (data.items.length > 0) {
-			photoHTML = '<ul>';
-			$.each(data.items, function(i, photos){
-			photoHTML += '<li class="col-sm-3 col-sm-3">';
-			photoHTML += '<a href=" ' + photos.link + ' " class="image">';
-			photoHTML += '<img src=" ' + photos.media.m + ' ">';
-			photoHTML += '</a>';
-			photoHTML += '</li>';
-			});
+// 	function flickrImages(data){
+// 		var photoHTML = "";
+// 		if (data.items.length > 0) {
+// 			photoHTML = '<ul>';
+// 			$.each(data.items, function(i, photos){
+// 			photoHTML += '<li class="col-sm-3 col-sm-3">';
+// 			photoHTML += '<a href=" ' + photos.link + ' " class="image">';
+// 			photoHTML += '<img src=" ' + photos.media.m + ' ">';
+// 			photoHTML += '</a>';
+// 			photoHTML += '</li>';
+// 			});
 
-		} else {
-		photoHTML =  photoHTML = '<p id="noMatch" class="lead text-danger"> No photos found to match your search word: ' + animal + '.</p>'
-		}
+// 		} else {
+// 		photoHTML =  photoHTML = '<p id="noMatch" class="lead text-danger"> No photos found to match your search word: ' + animal + '.</p>'
+// 		}
 
-		photoHTML += '</ul>';
+// 		photoHTML += '</ul>';
 
-		$("#photos").html(photoHTML);
-		}
+// 		$("#photos").html(photoHTML);
+// 		}
 
-		$.getJSON(flickrAPI, flickrOptions, flickrImages );
-	});
-}
+// 		$.getJSON(flickrAPI, flickrOptions, flickrImages );
+// 	});
+// }
 
 // End code for friends pics begin //
+////////////////////////////////////
+///////////////////////////////////
 
 $(document).ready(function(){
 	$("#searchPics").click(function(){
@@ -109,4 +113,25 @@ $(document).ready(function(){
 	return searchPictures();
 	});
 });
+
+	// function searchTerm(){
+	// 	if ($searchField.val === null){
+	// 		var searchHTML = "";
+	// 		searchHTML += '<p>Please enter a search word<p>';
+	// 	} else {
+	// 		return $searchField.val();
+	// 	}
+	// } 
+
+
+
+
+
+
+
+
+
+
+
+
 
