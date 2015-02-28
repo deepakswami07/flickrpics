7c241ae203e7f4220b15bb28fcc72f242a440206
daniel <deepakswami07@gmail.com>
2015-02-28 03:25:34 -0500

    add getjson for search friends

--- js/flickr.js
+++ js/flickr.js
@@ -8,13 +8,7 @@ var searchPictures = function() {
 
         event.preventDefault();
         var animal = $searchField.val();
-        var flickrOptions = {
-            tags: animal,
-            format: "json"
-        }
-
-        var animal = $searchField.val();
-
+        console.log(animal);
         var flickrOptions = {
             tags: animal,
             format: "json"
@@ -52,39 +46,35 @@ var searchPictures = function() {
 
 //Begin code for friends pics  //
 
+
 var friendsPictures = function() {
 
-    var apiKey = '9a204c1e5292bcbc81473e3ea47dd1d3';
-    
-    var flickrAPI = "https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key='+ apiKey +'jsoncallback? ";
+    var flickrAPI = "https://api.flickr.com/services/rest/??&method=flickr.people.getPublicPhotos&jsoncallback=?";
+
     $('form').submit(function(event) {
 
-        var $searchField = $("#search");
 
         event.preventDefault();
+        
+        var apiKey = '9a204c1e5292bcbc81473e3ea47dd1d3';
+        var $searchField = $("#searchFriends");
         var userId = $searchField.val();
+           
         var flickrOptions = {
-            tags: animal,
-            format: "json"
-        }
-
-        var $searchField = $('#search');
-        var animal = $searchField.val();
-
-        var flickrOptions = {
-            tags: animal,
-            tagmode: "all",
+            api_key: apiKey,
+            user_id: userId ,
+            safe_search: 1,
             format: "json"
         };
-
+        console.log(flickrOptions);
         function flickrImages(data) {
             var photoHTML = "";
-            if (data.items.length > 0) {
+            if (data.photos.length > 0) {
                 photoHTML = '<ul>';
-                $.each(data.items, function(i, photos) {
+                $.each(data.photos, function(i, photos) {
                     photoHTML += '<li class="col-sm-3 col-sm-3">';
-                    photoHTML += '<a href=" ' + photos.link + ' " class="image">';
-                    photoHTML += '<img src=" ' + photos.media.m + ' ">';
+                    photoHTML += '<a href=" ' + photos.owner + ' " class="image">';
+                    photoHTML += '<img src=" ' + photos.title + ' ">';
                     photoHTML += '</a>';
                     photoHTML += '</li>';
                 });
@@ -111,52 +101,6 @@ var friendsPictures = function() {
 
 var favoritePictures = function() {
 
-    var apiKey = '9a204c1e5292bcbc81473e3ea47dd1d3';
-    
-    var flickrAPI = "https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=";
-    $('form').submit(function(event) {
-
-        var $searchField = $("#search");
-
-        event.preventDefault();
-        var animal = $searchField.val();
-        var flickrOptions = {
-            tags: animal,
-            format: "json"
-        }
-
-        var $searchField = $('#search');
-        var animal = $searchField.val();
-
-        var flickrOptions = {
-            tags: animal,
-            tagmode: "all",
-            format: "json"
-        };
-
-        function flickrImages(data) {
-            var photoHTML = "";
-            if (data.items.length > 0) {
-                photoHTML = '<ul>';
-                $.each(data.items, function(i, photos) {
-                    photoHTML += '<li class="col-sm-3 col-sm-3">';
-                    photoHTML += '<a href=" ' + photos.link + ' " class="image">';
-                    photoHTML += '<img src=" ' + photos.media.m + ' ">';
-                    photoHTML += '</a>';
-                    photoHTML += '</li>';
-                });
-
-            } else {
-                photoHTML = photoHTML = '<p id="noMatch" class="lead text-danger"> No photos found to match your search word: ' + animal + '.</p>'
-            }
-
-            photoHTML += '</ul>';
-
-            $("#photos").html(photoHTML);
-        }
-
-        $.getJSON(flickrAPI, flickrOptions, flickrImages);
-    });
 }
 
 // end code for favorite pics //
