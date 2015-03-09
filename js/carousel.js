2d31bd9f9bb3db199ff4b1f36265851706bdf344
daniel <deepakswami07@gmail.com>
2015-03-09 04:23:46 -0400

    add lightbox call carousel.js

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+////////////////////////////
+//begin lightbox
+
+$(document).ready(function(){
+	$( "#imageGallery a" ).click(function( event ) {
+  		event.preventDefault();
+	});
+});
+
+//end slightbox
+/////////////////////////////
