c76ac203e4064586f313d7f1af892b95ddf1a8ed
daniel <deepakswami07@gmail.com>
2015-03-10 03:44:37 -0400

    comment out all code carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,60 @@
+	// $("body").delegate("a", "click", function(event) { 
+ //    event.preventDefault();
+ //    });
 ////////////////////////////
 //begin lightbox
 
-$(document).ready(function(){
-	$( "#imageGallery a" ).click(function( event ) {
-  		event.preventDefault();
-	});
-});
+// $("#getPictures").click(function(){
+// 	return pictures();
+// 	$("#imageGallery a").click(function(event){
+// 		event.preventDefault();
+// 	});
+// });
+
+// $("#imageGallery").on('click', "#imageGallery a", function(){
+// 	$("#imageGallery .photos").click(function(event){
+// 		event.preventDefault();
+// 	});
+// });
+
 
 //end slightbox
 /////////////////////////////
+
+// $("#imageGallery").load(function(){
+// 	$("#imageGallery a").click(function(event){
+// 		event.preventDefault();
+// 	});
+// });
+
+
+// $("#imageGallery a").click(function(event){
+// 	$("#imageGallery").attr('src', $(this).attr('href'));
+// 	event.preventDefault();
+// });
+
+
+// $(pictures).ajaxSuccess(function(){
+//     loadAll()
+
+    // $("#imageGallery a.class").click(function(event){
+    //     event.preventDefault();
+    // });
+// });
+
+
+// function callBack(){
+// $("#imageGallery a").click(function(event){
+// 		event.preventDefault();
+// 	});
+// }
+// document.getElementById("imageGallery").addEventListener('click', callback, true);
+
+// loadAll();
+// $("#imageGallery").delegate("#imageGallery a", "click", function(event){
+// 	event.preventDefault();
+// });
+
+//  $("#imageGallery").delegate("a", "click", function(event){
+//                 event.preventDefault();
+//  });
