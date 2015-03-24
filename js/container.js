9c949e54392ec6a09b599099fcec1ccea4cd790e
daniel <deepakswami07@gmail.com>
2015-03-24 16:47:08 -0400

    rewrite description and change id for each page

--- js/container.js
+++ js/container.js
@@ -3,10 +3,10 @@
 var $picturesHTML = " ";
 $picturesHTML += '<div class="">';
 $picturesHTML += '<section>';
-$picturesHTML += '<h1 class="main-header">GENERATE</h1>';
-$picturesHTML += '<h1 class="second-header-picutres">random pictures</h1>';
+$picturesHTML += '<h1 class="main-header">PICTURES</h1>';
+$picturesHTML += '<h1 class="second-header-picutres">pics of the day</h1>';
 $picturesHTML += '<article>';
-$picturesHTML += '<p class="description lead">Click on the generate pics button to see recent photos that have been posted on Flickr.</p>';
+$picturesHTML += '<p id="d-picture">Click on the button below to see the most interesting pictures of the day.</p>';
 $picturesHTML += '</article>';
 $picturesHTML += '</section>';
 $picturesHTML += '<section>';
@@ -16,9 +16,9 @@ $picturesFormHTML += '<form class="navbar-form navbar-center" role="picutres">';
 $picturesFormHTML += '<div class="form-group">';
 $picturesFormHTML += '<label for="getPictures"></label>';
 $picturesFormHTML += '<div class="form-group">';
-$picturesFormHTML += ' <button id="getPictures" type="submit" value="pictures">';
-$picturesFormHTML += '<span>get pics   </span>';
-$picturesFormHTML += '<span class="glyphicon glyphicon-search"></span>';
+$picturesFormHTML += ' <button id="getPictures" type="submit" value="pictures" style="text-align:center">';
+$picturesFormHTML += '<span>CLICK   </span>';
+$picturesFormHTML += '<span class="glyphicon glyphicon-download"></span>';
 $picturesFormHTML += '</button>';
 $picturesFormHTML += '</div>';
 $picturesFormHTML += '</div>';
@@ -28,31 +28,30 @@ $picturesFormHTML += '</form>';
 
 
 //////////////////////////
-// begin ajax for search pics
-var $searchPicsHTML = " ";
-$searchPicsHTML += '<section>';
-$searchPicsHTML += '<h1 class="main-header">SEARCH</h1>';
-$searchPicsHTML += '<h1 class="second-header-search">pics by topic</h1>';
-$searchPicsHTML += '<article>';
-$searchPicsHTML += '<p class="description">Type in a search term to find the most recent photos that have been posted on Flickr.</p>';
-$searchPicsHTML += '</article>';
-$searchPicsHTML += '</section>';
+//begin ajax for for username
+var $usernameHTML = " ";
+$usernameHTML += '<section>';
+$usernameHTML += '<h1 class="main-header">SEARCH</h1>';
+$usernameHTML += '<h1 class="second-header-search">pics by topic</h1>';
+$usernameHTML += '<article>';
+$usernameHTML += '<p id="d-search">Type in a search term to find the most recent photos that have been posted on Flickr.</p>';
+$usernameHTML += '</article>';
+$usernameHTML += '</section>';
 
-var $searchPicsFormHTML = " ";
-$searchPicsFormHTML += '<form class="navbar-form navbar-center" role="search">';
-$searchPicsFormHTML += '<div class="form-group">';
-$searchPicsFormHTML += '<label for="search"></label>';
-$searchPicsFormHTML += '<div class="form-group">';
-$searchPicsFormHTML += '<input type="text" class="form-control" id="search" name="search" placeholder="search Pics">';
-$searchPicsFormHTML += ' <button class="button-style" id="submit" type="submit" value="search">';
-$searchPicsFormHTML += '<span class="glyphicon glyphicon-search"></span>';
-$searchPicsFormHTML += '</button>';
-$searchPicsFormHTML += '</div>';
-$searchPicsFormHTML += '</div>';
-$searchPicsFormHTML += '</form>';
-// end ajax for search pics
-///////////////////////////
-
+var $usernamePicsFormHTML = " ";
+$usernamePicsFormHTML += '<form class="navbar-form navbar-center form-inline" role="searchFriendsName">';
+$usernamePicsFormHTML += '<div class="form-group">';
+$usernamePicsFormHTML += '<label for="searchFriendsName"></label>';
+$usernamePicsFormHTML += '<div class="form-group">';
+$usernamePicsFormHTML += '<input type="text" class="form-control" id="searchFriendsName" name="searchFriendsName" placeholder="Type in seach term...">';
+$usernamePicsFormHTML += ' <button class="button-style" id="submit" type="submit" value="search">';
+$usernamePicsFormHTML += '<span class="glyphicon glyphicon-search"></span>';
+$usernamePicsFormHTML += '</button>';
+$usernamePicsFormHTML += '</div>';
+$usernamePicsFormHTML += '</div>';
+$usernamePicsFormHTML += '</form>';
+//end ajax for friends pics by username
+////////////////////////////
 
 ///////////////////////////
 // begin ajax for userid pics
@@ -61,7 +60,7 @@ $useridHTML += '<section>';
 $useridHTML += '<h1 class="main-header">FRIEND\'S</h1>';
 $useridHTML += '<h1 class="second-header-userid">pics by userid</h1>';
 $useridHTML += '<article>';
-$useridHTML += '<p class="description">Too busy to joint Flickr but you want to stay connected to your friends by viewing their recent pictures? Type in the name of your friend and see what their most recent posts are.</p>';
+$useridHTML += '<p id="d-id">Too busy to joint Flickr but you want to stay connected to your friends by viewing their recent pictures? Type in the name of your friend and see what their most recent posts are.</p>';
 $useridHTML += '</article>';
 $useridHTML += '</section>';
 
@@ -80,34 +79,6 @@ $useridPicsFormHTML += '</form>';
 //end ajax for userid pics
 ///////////////////////////
 
-
-//////////////////////////
-//begin ajax for for username
-var $usernameHTML = " ";
-$usernameHTML += '<section>';
-$usernameHTML += '<h1 class="main-header">FRIEND\'S</h1>';
-$usernameHTML += '<h1 class="second-header-username">by username</h1>';
-$usernameHTML += '<article>';
-$usernameHTML += '<p class="description">Too busy to joint Flickr but you want to stay connected to your friends by viewing their recent pictures? Type in the name of your friend and see what their most recent posts are.</p>';
-$usernameHTML += '</article>';
-$usernameHTML += '</section>';
-
-var $usernamePicsFormHTML = " ";
-$usernamePicsFormHTML += '<form class="navbar-form navbar-center form-inline" role="searchFriendsName">';
-$usernamePicsFormHTML += '<div class="form-group">';
-$usernamePicsFormHTML += '<label for="searchFriendsName"></label>';
-$usernamePicsFormHTML += '<div class="form-group">';
-$usernamePicsFormHTML += '<input type="text" class="form-control" id="searchFriendsName" name="searchFriendsName" placeholder="mypics">';
-$usernamePicsFormHTML += ' <button class="button-style" id="submit" type="submit" value="search">';
-$usernamePicsFormHTML += '<span class="glyphicon glyphicon-search"></span>';
-$usernamePicsFormHTML += '</button>';
-$usernamePicsFormHTML += '</div>';
-$usernamePicsFormHTML += '</div>';
-$usernamePicsFormHTML += '</form>';
-//end ajax for friends pics by username
-////////////////////////////
-
-
 ///////////////////////////
 // begin ajax for friends favorite pics
 var $favoritesHTML = " ";
@@ -115,7 +86,7 @@ $favoritesHTML += '<section>';
 $favoritesHTML += '<h1 class="main-header">FRIEND\'S</h1>';
 $favoritesHTML += '<h1 class="second-header-favorites">favorite pics</h1>';
 $favoritesHTML += '<article>';
-$favoritesHTML += '<p class="description lead">Keep up-to-date on your friends’ favorite pictures. Type in your friend’s userid and find their favorite photos.</p>';
+$favoritesHTML += '<p id="d-favorite">Type in your friend’s userid or username and find their favorite photos.</p>';
 $favoritesHTML += '</article>';
 $favoritesHTML += '</section>';
 
@@ -134,6 +105,9 @@ $favoritesPicsFormHTML += '</form>';
 // end ajax for favorite pics
 ///////////////////////////
 
+//////////////////////////
+// begin ajax for search pics
+
 
 //////////////////////////
 // begin ajax for about HTML
@@ -142,7 +116,7 @@ $aboutHTML += '<section>';
 $aboutHTML += '<h1 class="main-header">ABOUT</h1>';
 $aboutHTML += '<h1 class="second-header-about">flickrpics</h1>';
 $aboutHTML += '<article>';
-$aboutHTML += '<p class="description">Keep your finger on the pulse of the latest picture trends on flickr.  Search for the most recent posts on flickr by category, friends or your friend’s favorites.</p>';
+$aboutHTML += '<p id="d-about">Keep your finger on the pulse of the latest picture trends on flickr.  Search for the most recent posts on flickr by category, friends or your friend’s favorites.</p>';
 $aboutHTML += '</article>';
 $aboutHTML += '</section>';
 
@@ -157,13 +131,44 @@ var $home = " ";
 $home += '<section id="toFill">';
 $home += '<h1 class="main-header">FLICKRPICS</h1>';
 $home += '<article>';
-$home += '<p class="description">Click on pictures to see randon pictures or do search for pictures, friends pics or friends favorites....</p>';
+$home += '<p id="d-home">With just a click see interesting pictures of the day, search for pictures, friend"\s pics or friend"\s favorites....</p>';
 $home += '</article>';
 $home += '</section>';
 
 var $homePicsFormHTML = " ";
 // end ajax for about home
 ///////////////////////////
+// var $searchPicsHTML = " ";
+// $searchPicsHTML += '<section>';
+// $searchPicsHTML += '<h1 class="main-header">SEARCH</h1>';
+// $searchPicsHTML += '<h1 class="second-header-search">pics by topic</h1>';
+// $searchPicsHTML += '<article>';
+// $searchPicsHTML += '<p class="description">Type in a search term to find the most recent photos that have been posted on Flickr.</p>';
+// $searchPicsHTML += '</article>';
+// $searchPicsHTML += '</section>';
+
+// var $searchPicsFormHTML = " ";
+// $searchPicsFormHTML += '<form class="navbar-form navbar-center" role="search">';
+// $searchPicsFormHTML += '<div class="form-group">';
+// $searchPicsFormHTML += '<label for="search"></label>';
+// $searchPicsFormHTML += '<div class="form-group">';
+// $searchPicsFormHTML += '<input type="text" class="form-control" id="search" name="search" placeholder="search Pics">';
+// $searchPicsFormHTML += ' <button class="button-style" id="submit" type="submit" value="search">';
+// $searchPicsFormHTML += '<span class="glyphicon glyphicon-search"></span>';
+// $searchPicsFormHTML += '</button>';
+// $searchPicsFormHTML += '</div>';
+// $searchPicsFormHTML += '</div>';
+// $searchPicsFormHTML += '</form>';
+// end ajax for search pics
+///////////////////////////
+
+
+
+
+
+
+
+
 
 
 //////////////////////////
