2f517e3ed1f40276b21955467592cf55bab12fcd
daniel <deepakswami07@gmail.com>
2015-03-30 16:30:05 -0400

    rearrange and change search html page

--- js/container.js
+++ js/container.js
@@ -15,58 +15,47 @@ var $homePicsFormHTML = " ";
 
 /////////////////////////////////////////////
 // begin ajax for interesting pics of the day
-var $picturesHTML = " ";
-$picturesHTML += '<div class="">';
-$picturesHTML += '<section>';
-$picturesHTML += '<h1 class="main-header">PICTURES</h1>';
-$picturesHTML += '<h1 class="second-header-picutres">pics of the day</h1>';
-$picturesHTML += '<article>';
-$picturesHTML += '<p id="d-picture">Click on the button below to see the most interesting pictures of the day.</p>';
-$picturesHTML += '</article>';
-$picturesHTML += '</section>';
-$picturesHTML += '<section>';
-
 var $picturesFormHTML = " ";
 $picturesFormHTML += '<form class="navbar-form navbar-center" role="picutres">';
 $picturesFormHTML += '<div class="form-group">';
-$picturesFormHTML += '<label for="getPictures"></label>';
+$picturesFormHTML += '<label for="getPicturesOfTheDay"></label>';
 $picturesFormHTML += '<div class="form-group">';
-$picturesFormHTML += ' <button class="btn btn-info button-style"" id="getPictures" type="submit" value="pictures" style="text-align:center">';
+$picturesFormHTML += ' <button class="btn btn-info button-style"" id="getPicturesOfTheDay" type="submit" value="pictures" style="text-align:center">';
 $picturesFormHTML += '<span>CLICK TO SEE PICS OF THE DAY  </span>';
 $picturesFormHTML += '<span class="glyphicon glyphicon-download"></span>';
 $picturesFormHTML += '</button>';
 $picturesFormHTML += '</div>';
 $picturesFormHTML += '</div>';
 $picturesFormHTML += '</form>';
-/////////////////////////////////////////////
 // end ajax for interesting pics of the day
+/////////////////////////////////////////////
 
 
 ///////////////////////////////////
 //begin ajax for for search by text
-var $usernameHTML = " ";
-$usernameHTML += '<section>';
-$usernameHTML += '<h1 class="main-header">SEARCH</h1>';
-$usernameHTML += '<h1 class="second-header-search">pics by topic</h1>';
-$usernameHTML += '<article>';
-$usernameHTML += '<p id="d-search">Type in a search term to find the most recent photos that have been posted on Flickr.</p>';
-$usernameHTML += '</article>';
-$usernameHTML += '</section>';
+var $searchHTML = " ";
+$searchHTML += '<section>';
+$searchHTML += '<h1 class="main-header">SEARCH</h1>';
+$searchHTML += '<h1 class="second-header-search">pics by topic</h1>';
+$searchHTML += '<article>';
+$searchHTML += '<p id="d-search">Type in a search term to find the most recent photos that have been posted on Flickr.</p>';
+$searchHTML += '</article>';
+$searchHTML += '</section>';
 
-var $usernamePicsFormHTML = " ";
-$usernamePicsFormHTML += '<form class="navbar-form navbar-center form-inline" role="searchFriendsName">';
-$usernamePicsFormHTML += '<div class="form-group">';
-$usernamePicsFormHTML += '<label for="searchFriendsName"></label>';
-$usernamePicsFormHTML += '<div class="form-group">';
-$usernamePicsFormHTML += '<input type="text" class="form-control" id="searchFriendsName" name="searchFriendsName" placeholder="Type in seach term...">';
-$usernamePicsFormHTML += ' <button class="btn btn-info button-style" id="submit" type="submit" value="search">';
-$usernamePicsFormHTML += '<span class="glyphicon glyphicon-search"></span>';
-$usernamePicsFormHTML += '</button>';
-$usernamePicsFormHTML += '</div>';
-$usernamePicsFormHTML += '</div>';
-$usernamePicsFormHTML += '</form>';
+var $userSearchPicsFormHTML = " ";
+$userSearchPicsFormHTML += '<form class="navbar-form navbar-center form-inline" role="searchFriendsName">';
+$userSearchPicsFormHTML += '<div class="form-group">';
+$userSearchPicsFormHTML += '<label for="searchFriendsName"></label>';
+$userSearchPicsFormHTML += '<div class="form-group">';
+$userSearchPicsFormHTML += '<input type="text" class="form-control" id="searchFriendsName" name="searchFriendsName" placeholder="Type in seach term...">';
+$userSearchPicsFormHTML += ' <button class="btn btn-info button-style" id="submit" type="submit" value="search">';
+$userSearchPicsFormHTML += '<span class="glyphicon glyphicon-search"></span>';
+$userSearchPicsFormHTML += '</button>';
+$userSearchPicsFormHTML += '</div>';
+$userSearchPicsFormHTML += '</div>';
+$userSearchPicsFormHTML += '</form>';
+//end ajax for for search by text
 ///////////////////////////////////
-//begin ajax for for search by text
 
 
 /////////////////////////////
@@ -122,9 +111,6 @@ $favoritesPicsFormHTML += '</form>';
 // end ajax for favorite pics
 ////////////////////////////
 
-//////////////////////////
-// begin ajax for search pics
-
 
 ////////////////////////////
 // begin ajax for about HTML
@@ -133,15 +119,13 @@ $aboutHTML += '<section>';
 $aboutHTML += '<h1 class="main-header">ABOUT</h1>';
 $aboutHTML += '<h1 class="second-header-about">flickrpics</h1>';
 $aboutHTML += '<article>';
-$aboutHTML += '<p id="d-about">Keep your finger on the pulse of the latest picture trends on flickr.  Search for the most recent posts on flickr by category, friends or your friend’s favorites.</p>';
+$aboutHTML += '<p id="d-about">Keep your finger on the pulse of the latest picture trends on flickr.  Search for the most interesting pics of the day,search by category, friend’s id or your friend’s favorites.</p>';
 $aboutHTML += '</article>';
 $aboutHTML += '</section>';
 
 var $aboutPicsFormHTML = " ";
 // end ajax for about HTML
 ///////////////////////////
-
-
 
 
 /////////////////////////////////////////////
@@ -160,47 +144,31 @@ $loadImageGalleryHTML += '</div>';
 
 //////////////////////////
 // begin append to HTML //
-// $("#home").click(function(){
-//      document.getElementById("toFill").innerHTML = $home;
-//      document.getElementById("result").innerHTML = $homePicsFormHTML;
-//      document.getElementById("loadImageGallery").innerHTML = "";
-// });
 
 $("#home").click(function() {
     document.getElementById("toFill").innerHTML = $home;
     document.getElementById("result").innerHTML = $picturesFormHTML;
-    // document.getElementById("loadImageGallery").innerHTML = $loadImageGalleryHTML;
 }); 
 
-$("#searchPics").click(function() {
-    document.getElementById("toFill").innerHTML = $searchPicsHTML;
-    document.getElementById("result").innerHTML = $searchPicsFormHTML;
-    document.getElementById("loadImageGallery").innerHTML = $loadImageGalleryHTML;
+$("#search").click(function() {
+    document.getElementById("toFill").innerHTML = $searchHTML;
+    document.getElementById("result").innerHTML = $userSearchPicsFormHTML;
 });
 
 $("#userID").click(function() {
     document.getElementById("toFill").innerHTML = $useridHTML;
     document.getElementById("result").innerHTML = $useridPicsFormHTML;
-    document.getElementById("loadImageGallery").innerHTML = $loadImageGalleryHTML;
-});
-
-$("#username").click(function() {
-    document.getElementById("toFill").innerHTML = $usernameHTML;
-    document.getElementById("result").innerHTML = $usernamePicsFormHTML;
-    document.getElementById("loadImageGallery").innerHTML = $loadImageGalleryHTML;
 });
 
 $("#favorites").click(function() {
     document.getElementById("toFill").innerHTML = $favoritesHTML;
     document.getElementById("result").innerHTML = $favoritesPicsFormHTML;
-    document.getElementById("loadImageGallery").innerHTML = $loadImageGalleryHTML;
 });
 
 
 $("#about").click(function() {
     document.getElementById("toFill").innerHTML = $aboutHTML;
     document.getElementById("result").innerHTML = $aboutPicsFormHTML;
-    document.getElementById("loadImageGallery").innerHTML = "";
 });
 
 /// end append to HTML ///
